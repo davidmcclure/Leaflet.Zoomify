b5ce1521a770750dc884998606b19ed6f2ea2fbd
David McClure <davidwilliammcclure@gmail.com>
2015-05-09 14:30:44 -0700

    Retab.

--- L.TileLayer.Zoomify.js
+++ L.TileLayer.Zoomify.js
@@ -3,116 +3,119 @@
  */
 
 L.TileLayer.Zoomify = L.TileLayer.extend({
-	options: {
-		continuousWorld: true,
-		tolerance: 0.8
-	},
 
-	initialize: function (url, options) {
-		options = L.setOptions(this, options);
-		this._url = url;
+  options: {
+    continuousWorld: true,
+    tolerance: 0.8
+  },
 
-    	var imageSize = L.point(options.width, options.height),
-	    	tileSize = options.tileSize;
+  initialize: function (url, options) {
 
-    	this._imageSize = [imageSize];
-    	this._gridSize = [this._getGridSize(imageSize)];
+    options = L.setOptions(this, options);
+    this._url = url;
 
-        while (parseInt(imageSize.x) > tileSize || parseInt(imageSize.y) > tileSize) {
-        	imageSize = imageSize.divideBy(2).floor();
-        	this._imageSize.push(imageSize);
-        	this._gridSize.push(this._getGridSize(imageSize));
-        }
+    var imageSize = L.point(options.width, options.height);
+    var tileSize = options.tileSize;
 
-		this._imageSize.reverse();
-		this._gridSize.reverse();
+    this._imageSize = [imageSize];
+    this._gridSize = [this._getGridSize(imageSize)];
 
-        this.options.maxZoom = this._gridSize.length - 1;
-	},
+    while (parseInt(imageSize.x) > tileSize || parseInt(imageSize.y) > tileSize) {
+      imageSize = imageSize.divideBy(2).floor();
+      this._imageSize.push(imageSize);
+      this._gridSize.push(this._getGridSize(imageSize));
+    }
 
-	onAdd: function (map) {
-		L.TileLayer.prototype.onAdd.call(this, map);
+    this._imageSize.reverse();
+    this._gridSize.reverse();
 
-		var mapSize = map.getSize(),
-			zoom = this._getBestFitZoom(mapSize),
-			imageSize = this._imageSize[zoom],
-			center = map.options.crs.pointToLatLng(L.point(imageSize.x / 2, imageSize.y / 2), zoom);
+    this.options.maxZoom = this._gridSize.length - 1;
 
-		map.setView(center, zoom, true);
-	},
+  },
 
-	_getGridSize: function (imageSize) {
-		var tileSize = this.options.tileSize;
-		return L.point(Math.ceil(imageSize.x / tileSize), Math.ceil(imageSize.y / tileSize));
-	},
+  onAdd: function (map) {
+    L.TileLayer.prototype.onAdd.call(this, map);
 
-	_getBestFitZoom: function (mapSize) {
-		var tolerance = this.options.tolerance,
-			zoom = this._imageSize.length - 1,
-			imageSize, zoom;
+    var mapSize = map.getSize(),
+      zoom = this._getBestFitZoom(mapSize),
+      imageSize = this._imageSize[zoom],
+      center = map.options.crs.pointToLatLng(L.point(imageSize.x / 2, imageSize.y / 2), zoom);
 
-		while (zoom) {
-			imageSize = this._imageSize[zoom];
-			if (imageSize.x * tolerance < mapSize.x && imageSize.y * tolerance < mapSize.y) {
-				return zoom;
-			}			
-			zoom--;
-		}
+    map.setView(center, zoom, true);
+  },
 
-		return zoom;
-	},
+  _getGridSize: function (imageSize) {
+    var tileSize = this.options.tileSize;
+    return L.point(Math.ceil(imageSize.x / tileSize), Math.ceil(imageSize.y / tileSize));
+  },
 
-	_tileShouldBeLoaded: function (tilePoint) {
-		var gridSize = this._gridSize[this._map.getZoom()];
-		return (tilePoint.x >= 0 && tilePoint.x < gridSize.x && tilePoint.y >= 0 && tilePoint.y < gridSize.y);
-	},
+  _getBestFitZoom: function (mapSize) {
+    var tolerance = this.options.tolerance,
+      zoom = this._imageSize.length - 1,
+      imageSize, zoom;
 
-	_addTile: function (tilePoint, container) {
-		var tilePos = this._getTilePos(tilePoint),
-			tile = this._getTile(),
-			zoom = this._map.getZoom(),
-			imageSize = this._imageSize[zoom],
-			gridSize = this._gridSize[zoom],
-			tileSize = this.options.tileSize;
+    while (zoom) {
+      imageSize = this._imageSize[zoom];
+      if (imageSize.x * tolerance < mapSize.x && imageSize.y * tolerance < mapSize.y) {
+        return zoom;
+      }
+      zoom--;
+    }
 
-		if (tilePoint.x === gridSize.x - 1) {
-			tile.style.width = imageSize.x - (tileSize * (gridSize.x - 1)) + 'px';
-		} 
+    return zoom;
+  },
 
-		if (tilePoint.y === gridSize.y - 1) {
-			tile.style.height = imageSize.y - (tileSize * (gridSize.y - 1)) + 'px';			
-		} 
+  _tileShouldBeLoaded: function (tilePoint) {
+    var gridSize = this._gridSize[this._map.getZoom()];
+    return (tilePoint.x >= 0 && tilePoint.x < gridSize.x && tilePoint.y >= 0 && tilePoint.y < gridSize.y);
+  },
 
-		L.DomUtil.setPosition(tile, tilePos, L.Browser.chrome || L.Browser.android23);
+  _addTile: function (tilePoint, container) {
+    var tilePos = this._getTilePos(tilePoint),
+      tile = this._getTile(),
+      zoom = this._map.getZoom(),
+      imageSize = this._imageSize[zoom],
+      gridSize = this._gridSize[zoom],
+      tileSize = this.options.tileSize;
 
-		this._tiles[tilePoint.x + ':' + tilePoint.y] = tile;
-		this._loadTile(tile, tilePoint);
+    if (tilePoint.x === gridSize.x - 1) {
+      tile.style.width = imageSize.x - (tileSize * (gridSize.x - 1)) + 'px';
+    }
 
-		if (tile.parentNode !== this._tileContainer) {
-			container.appendChild(tile);
-		}
-	},
+    if (tilePoint.y === gridSize.y - 1) {
+      tile.style.height = imageSize.y - (tileSize * (gridSize.y - 1)) + 'px';
+    }
 
-	getTileUrl: function (tilePoint) {
-		return this._url + 'TileGroup' + this._getTileGroup(tilePoint) + '/' + this._map.getZoom() + '-' + tilePoint.x + '-' + tilePoint.y + '.jpg';
-	},
+    L.DomUtil.setPosition(tile, tilePos, L.Browser.chrome || L.Browser.android23);
 
-	_getTileGroup: function (tilePoint) {
-		var zoom = this._map.getZoom(),
-			num = 0,
-			gridSize;
+    this._tiles[tilePoint.x + ':' + tilePoint.y] = tile;
+    this._loadTile(tile, tilePoint);
 
-		for (z = 0; z < zoom; z++) {
-			gridSize = this._gridSize[z];
-			num += gridSize.x * gridSize.y; 
-		}	
+    if (tile.parentNode !== this._tileContainer) {
+      container.appendChild(tile);
+    }
+  },
 
-		num += tilePoint.y * this._gridSize[zoom].x + tilePoint.x;
-      	return Math.floor(num / 256);;
-	}
+  getTileUrl: function (tilePoint) {
+    return this._url + 'TileGroup' + this._getTileGroup(tilePoint) + '/' + this._map.getZoom() + '-' + tilePoint.x + '-' + tilePoint.y + '.jpg';
+  },
+
+  _getTileGroup: function (tilePoint) {
+    var zoom = this._map.getZoom(),
+      num = 0,
+      gridSize;
+
+    for (z = 0; z < zoom; z++) {
+      gridSize = this._gridSize[z];
+      num += gridSize.x * gridSize.y;
+    }
+
+    num += tilePoint.y * this._gridSize[zoom].x + tilePoint.x;
+    return Math.floor(num / 256);;
+  }
 
 });
 
 L.tileLayer.zoomify = function (url, options) {
-	return new L.TileLayer.Zoomify(url, options);
+  return new L.TileLayer.Zoomify(url, options);
 };
